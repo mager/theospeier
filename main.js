99744dc53959f0ed6194a0da9e143c3a7ecda958
mager <andrew+github@mager.co>
2016-05-02 23:00:41 -0700

    Isolate the close functionality

--- main.js
+++ main.js
@@ -1,34 +1,16 @@
 $(document).ready(function() {
-   $("#reach") .click (function(){
+   $("#reach").click (function(){
         $("#form").fadeIn();
     });
-   $("#playing") .click (function(){
-        $(".feature").hide();
-        $("#signature").hide();
-        $("#playPage").fadeIn();
+    $("#close").click (function(){
+        $("#form").fadeOut();
     });
-   $("#follow") .click (function(){
-        $(".feature").hide();
-        $("#signature").hide();
-        $("#playPage").fadeIn();
-        $("#followPage").fadeIn();
-    });
-    $(".close") .click (function(){
-        $(".page").hide();
-        $("#signature").fadeIn();
-        $(".feature").fadeIn();
-    });
-    $("#service") .click (function(){
-        $(".feature").hide();
-        $("#signature").hide();
-        $("#servicesPage").fadeIn();
-        $("#followPage").fadeIn();
-    });
+
 
     $(document).keyup(function(e) {
         if (e.keyCode == 27) {
-            console.log('hello');
+            console.log('in');
+            $('#form').hide();
         }
     });
-
 });
